f6ebe720e75fb75f60fdc0d97c98439bf629654b
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-Air-3.attlocal.net>
2018-10-10 22:52:20 -0400

    #4- Populating pseudocode

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,27 @@
 console.log('Working')
+//I'm not entirely sure about what we need function-wise just yet.
+//Lets brainstorm...
+
+//We need functions to hide our page sections, set to reveal and hide sections on certain
+//click events.
+
+//We need functions to take a value from the buttons we press equal to the letter pressed.
+
+//Buttons need to print character value into a form. 
+
+//We need to figure out the  '_ _ _ _ _' format. I don't know how we get it like that...
+
+//Create conditionals that specify the rules of the game
+
+    //Player chooses a letter > If letter is correct, print letter to form
+    // > If not, remove portion of health bar to symbolize a chance lost. 
+    //> Player chooses another letter. Is there a loop we can do to make this more DRY?
+
+    //If player guesses wrong 5 times, player loses (cue animation or image for game lose)
+    //If player fills form before 5 characters, player wins (cue animation/image for game win)
+    
+    //Kind of a stretch goal here: If the player answers correctly 3 times in a row, player 
+    //unlocks all vowels in the word OR unlocks an extra letter
+
+// Store categories and word choices in arrays. 
+// Make a smart randomizer.
